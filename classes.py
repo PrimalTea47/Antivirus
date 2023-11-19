edd2d856fa6db8421339234f0d21f2e2c872370b
PrimalTea47 <leandre.fouret@student-oec.fr>
2023-11-19 14:06:15 +0100

    Ajout de boutons Menu directement dans chaque niveaux pour permettre de retourner au Menu Trouver un moyen de mettre plusieurs Rect dans une image

--- classes.py
+++ classes.py
@@ -1,22 +1,42 @@
 import pygame
+pygame.init()
 
-screen = pygame.display.set_mode((800,800))
+font = pygame.font.SysFont('arialblack', 75)
+little_font = pygame.font.SysFont('arialblack', 30)
 
-rect1 = pygame.Rect(100,100,100,100)
-image = pygame.image.load('rectangle.png')
+#écriture de texte dans la fenêtre
+def write_text(text: str, font, color, x, y, fenetre):
+    img = font.render(text, True, color)
+    fenetre.blit(img, (x, y))
 
-run = True
-while run:
 
-    pygame.draw.rect(screen, (255,0,0), rect1)
-    screen.blit(image, (400,400), rect1)
 
-    for event in pygame.event.get():
 
-        if event.type == pygame.QUIT:
-            run = False
+class Restart:
+	def __init__(self,fenetre):
+		self.fenetre = fenetre
 
-    
-    pygame.display.flip()
+	def restart_button(self):
+		self.img_restart = pygame.image.load('assets/restart.png')
+		self.restart_button = pygame.transform.scale(self.img_restart,(100,100))
+		self.hit_restart_button = self.restart_button.get_rect(center=(700,100))
 
-pygame.quit()
+	def show_restart_button(self):
+		self.fenetre.blit(self.restart_button,self.hit_restart_button)
+
+	def get_rect_restart(self):
+		return self.hit_restart_button
+
+
+class Casa:
+	def __init__(self, fenetre):
+		self.fenetre = fenetre
+	def casa_button(self):
+		self.img_home = pygame.image.load('assets/home.png')
+		self.home_button = pygame.transform.scale(self.img_home, (100,100))
+		self.hit_home_button = self.home_button.get_rect(center=(600,100))
+	def show_home_button(self):
+		self.fenetre.blit(self.home_button, self.hit_home_button)
+	def get_rect_home(self):
+		return self.hit_home_button
+	
--- key.py
+++ key.py
@@ -1,109 +1,186 @@
 import pygame
-#from classes import *
+from classes import *
 
-screen = pygame.display.set_mode((800,800))
-pygame.display.set_caption('Test mouvements clavier')
+def menu():
+    pygame.init()
+    screen = pygame.display.set_mode((800,800))
+    pygame.display.set_caption('Sélectionnez votre niveau')
 
-background = pygame.image.load('assets/backgroundPlay.jpg')
-black_back = pygame.Rect(0,0,800,200)
+    #Bouton de niveaux
+    cadre_niveau_1 = pygame.Rect(290,140,230,70)
+    cadre_niveau_2 = pygame.Rect(290, 290, 230, 70)
+    cadre_niveau_3 = pygame.Rect(290, 440, 230, 70)
 
-bordure_1 = pygame.Rect(50,760,700,50)
-bordure_2 = pygame.Rect(640,200,50,600)
-bordure_3 = pygame.Rect(65,320,50,550)
-bordure_4 = pygame.Rect(200,190,700,50)
+    run = True
+    while run:
 
+        #ajout des boutons
+        pygame.draw.rect(screen, (0, 153, 51), cadre_niveau_1)
+        write_text('Niveau 1', font, (0, 0, 0), 300, 150, screen)
 
-#Block
-img_block = pygame.image.load('assets/molecule/block.png')
-img_block_hitbox = img_block.get_rect(center=(300,300))
+        pygame.draw.rect(screen, (255, 102, 0), cadre_niveau_2)
+        write_text('Niveau 2', font, (0,0,0), 300, 300, screen)
 
-#Red
-img_red = pygame.image.load('assets/molecule/3.png')
-img_red_hitbox = img_red.get_rect(center = (600,700))
+        pygame.draw.rect(screen, (200, 0, 0), cadre_niveau_3)
+        write_text('Niveau 3',font, (0,0,0), 300, 450, screen)
 
-#Purple
-img_purple = pygame.image.load('assets/molecule/5.png')
-img_purple_hitbox = img_purple.get_rect(center=(100,600))
+        for event in pygame.event.get():
 
-control_list = [img_red_hitbox, img_purple_hitbox]
-control_list_names = ['Red', 'Violet']
-collisions_list = control_list[0] or control_list[1]
-
-moving = 0
-
-run = True
-while run:
-
-    for event in pygame.event.get():
-
-        #Fermer la fenêtre
-        if event.type == pygame.QUIT:
-            run = False
-
-        #bouger les rectangles TOUT EN GÉRANT LES COLLISIONS
-        if event.type == pygame.KEYDOWN:
+            if event.type == pygame.QUIT:
+                run = False
             
-            if event.key == pygame.K_DOWN:
-                control_list[moving].x , control_list[moving].y  = control_list[moving].x + 5 , control_list[moving].y + 5
+            #Éxécution des niveaux
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if cadre_niveau_1.collidepoint(event.pos):
+                    niveau_1()
+    
+        pygame.display.update()
+
+
+    pygame.quit()
+
+
+
+
+def niveau_1():
+    pygame.init()
+    screen = pygame.display.set_mode((800,800))
+    pygame.display.set_caption('NIVEAU 1')
+
+    #Autres variables
+    background = pygame.image.load('assets/backgroundPlay.jpg')
+    black_back = pygame.Rect(0,0,800,200)
+
+    #Bordures pour ne pas sortie du plateau de jeu
+    bordure_1 = pygame.Rect(50,760,700,50)
+    bordure_2 = pygame.Rect(640,200,50,600)
+    bordure_3 = pygame.Rect(65,320,50,550)
+    bordure_4 = pygame.Rect(200,190,700,50)
+    
+
+
+    #Block
+    img_block = pygame.image.load('assets/molecule/block.png')
+    img_block_hitbox = img_block.get_rect(center=(300,300))
+
+    #Red
+    img_red = pygame.image.load('assets/molecule/3.png')
+    img_red_hitbox = img_red.get_rect(center = (600,700))
+
+    #Purple
+    img_purple = pygame.image.load('assets/molecule/5.png')
+    img_purple_hitbox = img_purple.get_rect(center=(100,600))
+
+    #Restart & Home
+    recommencer = Restart(screen)
+    recommencer.restart_button()
+    retour_menu = Casa(screen)
+    retour_menu.casa_button()
+
+    control_list = [img_red_hitbox, img_purple_hitbox]
+    control_list_names = ['Rouge', 'Violet']
+    control_color = [(255,0,0),(153, 0, 255)]
+
+
+
+    moving = 0
+
+    run = True
+    while run:
+
+
+
+        for event in pygame.event.get():
+
+            
+
+            #Fermer la fenêtre
+            if event.type == pygame.QUIT:
+                run = False
+            
+
+            #Besoin de recommencer le niveau OU retourner au menu
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if recommencer.get_rect_restart().collidepoint(event.pos):
+                    niveau_1()
                 
-                if img_block_hitbox.collidelistall(control_list):
-                    control_list[moving].x , control_list[moving].y  = control_list[moving].x - 5 , control_list[moving].y - 5
+                if retour_menu.get_rect_home().collidepoint(event.pos):
+                    menu()
+
             
-            
-            if event.key == pygame.K_UP:
-                control_list[moving].x , control_list[moving].y  = control_list[moving].x - 5 , control_list[moving].y - 5
+
+            #bouger les rectangles TOUT EN GÉRANT LES COLLISIONS
+            if event.type == pygame.KEYDOWN:
                 
-                if img_block_hitbox.collidelistall(control_list):
-                    control_list[moving].x , control_list[moving].y  = control_list[moving].x + 5 , control_list[moving].y + 5
+                if event.key == pygame.K_DOWN:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y + 10
+                    
+                    if img_block_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_purple_hitbox, img_red_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y - 10
+                
+                
+                if event.key == pygame.K_UP:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y - 10
+                    
+                    if img_block_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_purple_hitbox, img_red_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y + 10
+                        
+                    
+                if event.key == pygame.K_RIGHT:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y - 10            
+                    
+                    if img_block_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_purple_hitbox, img_red_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y + 10 
+                
+                
+                if event.key == pygame.K_LEFT:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y + 10
+                    
+                    if img_block_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_purple_hitbox, img_red_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y - 10
                     
                 
-            if event.key == pygame.K_RIGHT:
-                control_list[moving].x , control_list[moving].y  = control_list[moving].x + 5 , control_list[moving].y - 5            
-                
-                if img_block_hitbox.collidelistall(control_list):
-                    control_list[moving].x , control_list[moving].y  = control_list[moving].x - 5 , control_list[moving].y + 5 
-            
-            
-            if event.key == pygame.K_LEFT:
-                control_list[moving].x , control_list[moving].y  = control_list[moving].x - 5 , control_list[moving].y + 5
-                
-                if img_block_hitbox.collidelistall(control_list):
-                    control_list[moving].x , control_list[moving].y  = control_list[moving].x + 5 , control_list[moving].y - 5
+                #gérer le changement de rectangles si '(CTRL) droit' est appuyé
+                if event.key == pygame.K_RCTRL:
+                    if moving == len(control_list)-1:
+                        moving = 0
+                    else:
+                        moving += 1
+                        
                 
             
-            #gérer le changement de rectangles si '(CTRL) droit' est appuyé
-            if event.key == pygame.K_RCTRL:
-                if moving == len(control_list)-1:
-                    moving = 0
-                    print(control_list_names[moving])
-                else:
-                    moving += 1
-                    print(control_list_names[moving])
-            
-        
-        #Le but du jeu est atteint
-        if pygame.Rect.colliderect(img_red_hitbox, black_back):
-            run = False
+            #Le but du jeu est atteint
+            if pygame.Rect.colliderect(img_red_hitbox, black_back):
+                menu()
+                              
                 
             
-        
-        
-        #tout le temps mettre à jour l'écran
-        screen.blit(background, (-100,200))
-        pygame.draw.rect(screen, (0,0,0), black_back)
-        #afficher les images
-        screen.blit(img_block, img_block_hitbox)
-        screen.blit(img_red, img_red_hitbox)
-        screen.blit(img_purple, img_purple_hitbox)
+            
+            #tout le temps mettre à jour l'écran
+            screen.blit(background, (-100,200))
+            pygame.draw.rect(screen, (0,0,0), black_back)
+            write_text(control_list_names[moving], font, (control_color[moving]), 0, 0, screen)
+            write_text('CTRL droit pour changer', little_font, control_color[moving], 0, 50, screen)
 
-        #bordures
-        pygame.draw.rect(screen, (255,0,0), bordure_1)
-        pygame.draw.rect(screen, (255,0,0), bordure_2)
-        pygame.draw.rect(screen, (255,0,0), bordure_3)
-        pygame.draw.rect(screen, (255,0,0), bordure_4)
+            #afficher les images
+            screen.blit(img_block, img_block_hitbox)
+            screen.blit(img_red, img_red_hitbox)
+            screen.blit(img_purple, img_purple_hitbox)
+            recommencer.show_restart_button()
+            retour_menu.show_home_button()
+
+            #bordures
+            pygame.draw.rect(screen, (255,0,0), bordure_1)
+            pygame.draw.rect(screen, (255,0,0), bordure_2)
+            pygame.draw.rect(screen, (255,0,0), bordure_3)
+            pygame.draw.rect(screen, (255,0,0), bordure_4)
 
             
-    
-    pygame.display.update()
 
-pygame.quit()
+                
+        
+        pygame.display.update()
+
+    pygame.quit()
+
+menu()
