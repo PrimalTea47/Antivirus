54710617f6f6908af6a74847123abf5094b19554
PrimalTea47 <leandre.fouret@student-oec.fr>
2023-11-21 15:12:51 +0100

    ajout du niveau 3

--- key.py
+++ key.py
@@ -1,6 +1,7 @@
 import pygame
 from classes import *
 
+####MENU####
 def menu():
     pygame.init()
     screen = pygame.display.set_mode((800,800))
@@ -10,6 +11,7 @@ def menu():
     cadre_niveau_1 = pygame.Rect(290,140,230,70)
     cadre_niveau_2 = pygame.Rect(290, 290, 230, 70)
     cadre_niveau_3 = pygame.Rect(290, 440, 230, 70)
+    cadre_quit = pygame.Rect(310, 590, 190,70)
 
     run = True
     while run:
@@ -23,6 +25,9 @@ def menu():
 
         pygame.draw.rect(screen, (200, 0, 0), cadre_niveau_3)
         write_text('Niveau 3',font, (0,0,0), 300, 450, screen)
+
+        pygame.draw.rect(screen, (100,100,100), cadre_quit)
+        write_text('Quitter', font, (0,0,0), 320,600, screen)
 
         for event in pygame.event.get():
 
@@ -38,7 +43,10 @@ def menu():
                     niveau_2()
                 
                 if cadre_niveau_3.collidepoint(event.pos):
-                    pygame.display.set_caption('NIVEAU 3 INDISPONIBLE POUR LE MOMENT')
+                    niveau_3()
+                
+                if cadre_quit.collidepoint(event.pos):
+                    run = False
                     
                     
     
@@ -46,9 +54,12 @@ def menu():
 
 
     pygame.quit()
-
-
-
+####FIN MENU####
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
 ####NIVEAU 1####
 def niveau_1():
     pygame.init()
@@ -195,11 +206,11 @@ def niveau_1():
     pygame.quit()
 
 ####FIN NIVEAU 1####
-
-
-
-
-
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
 ####NIVEAU 2####
 def niveau_2():
     screen = pygame.display.set_mode((800,800))
@@ -316,6 +327,145 @@ def niveau_2():
             pygame.draw.rect(screen, (0,0,255), img_block_hitbox, 2)
             pygame.draw.rect(screen, (0,0,255), img_block2_hitbox, 2)
         
+        pygame.display.update()
+    pygame.quit()
+
+####FIN NIVEAU 2####
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+######################################################################################################################
+####NIVEAU 3####
+def niveau_3():
+    screen = pygame.display.set_mode((800,800))
+    pygame.display.set_caption('NIVEAU 3')
+
+    #Autres variables
+    background = pygame.image.load('assets/backgroundPlay.jpg')
+    black_back = pygame.Rect(0,0,800,200)
+
+    #Block
+    img_block = pygame.image.load('assets/molecule/block.png')
+    img_block_hitbox = img_block.get_rect(center=(380,350))
+
+    #Block 2
+    img_block2_hitbox = img_block.get_rect(center=(380,490))
+
+    #Red
+    img_red = pygame.image.load('assets/molecule/3.png')
+    img_red_hitbox = img_red.get_rect(center = (565,700))
+
+    #Dark Blue
+    img_darkBlue = pygame.image.load('assets/molecule/6.png')
+    img_darkBlue_hitbox = img_darkBlue.get_rect(center=(190,390))
+
+    #Orange
+    img_orange = pygame.image.load('assets/molecule/9.png')
+    img_orange_hitbox = img_orange.get_rect(center=(0,700))
+
+    #Light Green
+    img_lightGreen = pygame.image.load('assets/molecule/1.png')
+    img_lightGreen_hitbox = img_lightGreen.get_rect(center=(700,400))
+
+    #Restart & Home
+    recommencer = Restart(screen)
+    recommencer.restart_button()
+    retour_menu = Casa(screen)
+    retour_menu.casa_button()
+
+    control_list = [img_red_hitbox, img_darkBlue_hitbox, img_orange_hitbox, img_lightGreen_hitbox]
+    control_list_names = ['Rouge', 'Bleu foncé', 'Orange', 'Vert clair']
+    control_color = [(255,0,0),(0, 0, 255), (217, 95, 9), (123, 191, 46)]
+
+    moving = 0
+    
+
+    run = True
+    while run:
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+            
+            #Besoin de recommencer le niveau OU retourner au menu
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                if recommencer.get_rect_restart().collidepoint(event.pos):
+                    niveau_3()
+                
+                if retour_menu.get_rect_home().collidepoint(event.pos):
+                    menu()
+
+            
+
+            #bouger les rectangles TOUT EN GÉRANT LES COLLISIONS
+            if event.type == pygame.KEYDOWN:
+                
+                if event.key == pygame.K_DOWN:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y + 10
+                    
+                    if img_block_hitbox.collidelistall(control_list) or img_block2_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_red_hitbox) or pygame.Rect.colliderect(img_red_hitbox,img_darkBlue_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_lightGreen_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_lightGreen_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_orange_hitbox, img_darkBlue_hitbox) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_lightGreen_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y - 10
+                
+                
+                if event.key == pygame.K_UP:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y - 10
+                    
+                    if img_block_hitbox.collidelistall(control_list) or img_block2_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_red_hitbox) or pygame.Rect.colliderect(img_red_hitbox,img_darkBlue_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_lightGreen_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_lightGreen_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_orange_hitbox, img_darkBlue_hitbox) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_lightGreen_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y + 10
+                        
+                    
+                if event.key == pygame.K_RIGHT:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y - 10            
+                    
+                    if img_block_hitbox.collidelistall(control_list) or img_block2_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_red_hitbox) or pygame.Rect.colliderect(img_red_hitbox,img_darkBlue_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_lightGreen_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_lightGreen_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_orange_hitbox, img_darkBlue_hitbox) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_lightGreen_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y + 10 
+                
+                
+                if event.key == pygame.K_LEFT:
+                    control_list[moving].x , control_list[moving].y  = control_list[moving].x - 10 , control_list[moving].y + 10
+                    
+                    if img_block_hitbox.collidelistall(control_list) or img_block2_hitbox.collidelistall(control_list) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_red_hitbox) or pygame.Rect.colliderect(img_red_hitbox,img_darkBlue_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_lightGreen_hitbox) or pygame.Rect.colliderect(img_red_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_lightGreen_hitbox, img_orange_hitbox) or pygame.Rect.colliderect(img_orange_hitbox, img_darkBlue_hitbox) or pygame.Rect.colliderect(img_darkBlue_hitbox, img_lightGreen_hitbox):
+                        control_list[moving].x , control_list[moving].y  = control_list[moving].x + 10 , control_list[moving].y - 10
+                    
+                
+                #gérer le changement de rectangles si '(CTRL) droit' est appuyé
+                if event.key == pygame.K_RCTRL:
+                    if moving == 3:
+                        moving = 0
+                    else:
+                        moving += 1
+                        
+                
+            
+            #Le but du jeu est atteint
+            if pygame.Rect.colliderect(img_red_hitbox, black_back):
+                menu()
+            
+            #tout le temps mettre à jour l'écran
+            screen.blit(background, (-100,200))
+            pygame.draw.rect(screen, (0,0,0), black_back)
+            write_text(control_list_names[moving], font, (control_color[moving]), 0, 0, screen)
+            write_text('CTRL droit pour changer', little_font, control_color[moving], 0, 50, screen)
+
+            #afficher les images
+            screen.blit(img_block, img_block_hitbox)
+            screen.blit(img_block, img_block2_hitbox)
+            screen.blit(img_red, img_red_hitbox)
+            screen.blit(img_darkBlue, img_darkBlue_hitbox)
+            screen.blit(img_orange, img_orange_hitbox)
+            screen.blit(img_lightGreen, img_lightGreen_hitbox)
+            recommencer.show_restart_button()
+            retour_menu.show_home_button()
+            pygame.draw.rect(screen, (0,0,255), img_red_hitbox, 2)
+            pygame.draw.rect(screen, (0,0,255), img_darkBlue_hitbox, 2)
+            pygame.draw.rect(screen, (0,0,255), img_block_hitbox, 2)
+            pygame.draw.rect(screen, (0,0,255), img_block2_hitbox, 2)
+            pygame.draw.rect(screen, (0,0,255), img_orange_hitbox, 2)
+            pygame.draw.rect(screen, (0,0,255), img_lightGreen_hitbox, 2)
+
+
+
         pygame.display.update()
     pygame.quit()
 
